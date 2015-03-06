89438942fc1fa04468904378d5e2cb0c26dfd53a
Dean151 <thomas.durand.verjat@gmail.com>
2015-03-06 11:32:13 +0100

    Reading correspondant points from input file

--- Project/src/solveCamera/solveCamera.cpp
+++ Project/src/solveCamera/solveCamera.cpp
@@ -3,8 +3,8 @@ Solve Camera algorithm
 Programmed in 2015
 
 USAGE :
-$ solveCamera calibration.yaml inputPoints.txt out.yaml
-$ solveCamera ../../out/calibrationResults/30_02.yaml inputPoints.txt out.yaml
+$ ./solveCamera.out calibration.yaml inputPoints.txt out.yaml
+$ ./solveCamera.out ../../out/calibrationResults/30_02.yaml ../../data/frames.txt out.yaml
 */
 
 #include "opencv2/core/core.hpp"       // Mat, Point2f
@@ -13,6 +13,12 @@ $ solveCamera ../../out/calibrationResults/30_02.yaml inputPoints.txt out.yaml
 #include "opencv2/calib3d/calib3d.hpp" // (find|draw)ChessboardCorners
 #include <string>                      // to_string
 #include <iostream>                    // cout, cerr
+#include <fstream>					   // Read files
+#include <vector>
+#include <sstream>
+#include <utility>
+
+std::vector<std::string> explode(std::string const & s, char delim);
 
 using namespace cv;
 using namespace std;
@@ -28,11 +34,9 @@ int main( int argc, const char** argv )
 		return 1;
 	}
 
+	// Getting args
 	string calibrationFile(argv[1]);
-
-	// FIXME use an input file for points
-	string inputPoints(argv[2]); // NOT USED YET
-	
+	string inputPointsFile(argv[2]);	
 	string outYamlFile(argv[3]);
 
 	FileStorage fs(calibrationFile, FileStorage::READ);
@@ -41,14 +45,17 @@ int main( int argc, const char** argv )
 		return 1;
 	}
 
-	FileStorage out(outYamlFile, FileStorage::WRITE);
-	if (!out.isOpened()) {
-		cerr << "The specified calibration file is not valid" << endl;
+	ifstream pointFile(inputPointsFile);  // read only opening
+	if (!pointFile) {
+		cerr << "The specified input point file could not be opened" << endl;
 		return 1;
 	}
 
-	vector<Point2f> imagePoints;
-	vector<Point3f> objectPoints;
+	FileStorage out(outYamlFile, FileStorage::WRITE);
+	if (!out.isOpened()) {
+		cerr << "The specified out file could not be opened or created" << endl;
+		return 1;
+	}
 
 	// Getting camera matrix from calibration file
 	Mat cameraMatrix;
@@ -67,27 +74,81 @@ int main( int argc, const char** argv )
 	rvec.at<double>(1) = 200;
 	rvec.at<double>(2) = 0;
 
-	// FIXME read points from input file, this is dirty
-	 imagePoints.push_back(Point2f(456,352));
-	objectPoints.push_back(Point3f(-225.7404,-230,0));
-	 imagePoints.push_back(Point2f(456,87));
-	objectPoints.push_back(Point3f(-225.7404,-230.5175,15.98));
-	//  imagePoints.push_back(Point2f(352,335));
-	// objectPoints.push_back(Point3f(-225.69119,-205.60655,0));
-	//  imagePoints.push_back(Point2f(350,178));
-	// objectPoints.push_back(Point3f(-225.69119,-205.60655,19.317));
-	 imagePoints.push_back(Point2f(222,355));
-	objectPoints.push_back(Point3f(-245.4893,-226.48495,0));
-	 imagePoints.push_back(Point2f(224,76));
-	objectPoints.push_back(Point3f(-245.4893,-226.48495,16.99));
+	// Read corresponding points for solvePnp calculation
+	vector<Point2f> imagePoints;
+	vector<Point3f> objectPoints;
 
+	vector<string> coords;
+	int currentFrame = -1;
+	string line;
+	int mode = -1; // 0 for new frame, 1 for cam points, 2 for obj points
 
-	// Calculating solution
-	solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec, false, CV_P3P);
+	while(getline(pointFile, line)) {
+		if (line.compare(0, 5, "frame") == 0) {
+			mode = 0;
+			continue;
+		}
+		if (line.compare(0, 3, "cam") == 0) {
+			mode = 1;
+			continue;
+		}
+		if (line.compare(0, 3, "obj") == 0) {
+			mode = 2;
+			continue;
+		}
+		if (line.compare(0, 3, "end") == 0) {
+			mode = 3;
+			continue;
+		}
 
-	// Writing solution
-	out << "rvec" << rvec;
-	out << "tvec" << tvec;
+		switch (mode) {
+			case 0:
+				currentFrame = std::stoi(line);
+				mode = -1;
+				break;
+			case 1:
+				coords = explode(line, ' ');
+				imagePoints.push_back(Point2f(std::stod(coords[0]), std::stod(coords[1])));
+				break;
+			case 2:
+				coords = explode(line, ' ');
+				objectPoints.push_back(Point3f(std::stod(coords[0]), std::stod(coords[1]), std::stod(coords[2])));
+				break;
+			case 3:
+				if (imagePoints.size() == objectPoints.size() && objectPoints.size() >= 4) {
+					// Calculating solution
+					solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec, false, CV_P3P);
+					// FIXME solvePnp does not like to have more than 4 points
+
+					// Writing solution
+					out << "frame" << currentFrame << "rvec" << rvec;
+					out << "frame" << currentFrame << "tvec" << tvec;
+				} else {
+					cerr << "Frame number " << currentFrame << " was ignored" << endl;
+				}
+
+				imagePoints.clear();
+				objectPoints.clear();
+
+				mode = -1;
+				currentFrame = -1;
+				break;
+		}
+	}
 
 	return 0;
+}
+
+// Explode function
+std::vector<std::string> explode(std::string const & s, char delim)
+{
+    std::vector<std::string> result;
+    std::istringstream iss(s);
+
+    for (std::string token; std::getline(iss, token, delim); )
+    {
+        result.push_back(std::move(token));
+    }
+
+    return result;
 }
