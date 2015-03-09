766dfa551dd91d16254ed8575e8523eedd671d3d
loic <loic.padier@gmail.com>
2015-03-09 15:07:16 +0100

    removed python from provision.sh

--- provision.sh
+++ provision.sh
@@ -7,14 +7,5 @@ pacman -Sqyu --noconfirm
 # install opencv
 pacman -Sq   --noconfirm opencv
 
-# install numpy python2 aka opencv binder
-pacman -Sq   --noconfirm python2-numpy
-
 # disable 1394 driver
 ln /dev/null /dev/raw1394
-
-# remove symlink to python3
-rm /usr/bin/python
-
-# and replace it with symlink to python2
-ln -s /usr/bin/python2 /usr/bin/python
