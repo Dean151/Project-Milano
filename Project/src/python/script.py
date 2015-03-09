355f9a22431b8f7ea6e12558c51e0c4bc8c48d3b
Thomas Buick <zogwarg@gmail.com>
2015-03-10 00:12:07 +0100

    Added a lot of FIXMEs (a lot of stuff is hardcoded), and corrected some names

--- Project/src/python/script.py
+++ Project/src/python/script.py
@@ -4,10 +4,11 @@ import bpy
 bpy.ops.object.select_all(action="SELECT")
 bpy.ops.object.delete()
 
-# Importing STL
+# Importing STL - FIXME should be a parameter
 bpy.ops.import_mesh.stl(filepath="/vagrant/Project/data/GarRep.stl")
 
-# Alias for scene, needed for enabling camera and sun
+# Alias for scene, needed for enabling camera and sun 
+# FIXME resolution should be a parameter
 scn = bpy.context.scene
 scn.render.resolution_x = 720
 scn.render.resolution_y = 576
@@ -16,12 +17,12 @@ scn.render.pixel_aspect_x = 1
 
 
 # Creating camera
-cam = bpy.data.cameras.new("magic")
-obj = bpy.data.objects.new("magic", cam)
+cam = bpy.data.cameras.new("My camera")
+obj = bpy.data.objects.new("My camera", cam)
 scn.objects.link(obj)
 scn.camera = obj
 
-# Camera data should be given by yaml parse
+# FIXME Camera data should be given by yaml parse
 obj.rotation_mode = 'AXIS_ANGLE'
 obj.rotation_axis_angle[0] = 1.6775949647875288e+00
 obj.rotation_axis_angle[1] = 1.6394247253724927e+00
@@ -31,6 +32,7 @@ obj.location.x = -2.7112076513151294e+02
 obj.location.y = -3.7260196389150241e+02
 obj.location.z = -9.3188488777097902e+00
 
+# FIXME should camera intrinsic data be hardcoded ?
 cam.lens = 7.5
 cam.sensor_width = 6.54
 cam.sensor_height = 4.89
@@ -38,11 +40,11 @@ cam.sensor_height = 4.89
 cam.clip_end = 500
 
 # Creating sun, at our back
-sun = bpy.data.lamps.new("sun", type="SUN")
-obj = bpy.data.objects.new("sun", sun)
+sun = bpy.data.lamps.new("sunshine", type="SUN")
+obj = bpy.data.objects.new("sunshine", sun)
 scn.objects.link(obj)
 
-# Sun data should be given by yaml parse, or set to be constant,
+# FIXME Sun data should be given by yaml parse, or set to be constant,
 # or different kind of render should be used
 obj.rotation_mode = 'AXIS_ANGLE'
 obj.rotation_axis_angle[0] = 1.6775949647875288e+00
@@ -50,6 +52,6 @@ obj.rotation_axis_angle[1] = 1.6394247253724927e+00
 obj.rotation_axis_angle[2] =-2.7313663567392477e-01
 obj.rotation_axis_angle[3] =-2.2805221764000944e-01
 
-#Output Render
+#Output Render - FIXME path should be a parameter
 bpy.ops.render.render()
 bpy.data.images[0].save_render("/vagrant/Project/out/blenderFrames/frame.png")
